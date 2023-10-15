e98f3bf6c9a13fae92f452e0a2570dc9108ead41
DESKTOP-CMMBAAD\spcom <saponp>
2023-10-15 14:23:26 +0700

    add spring boot3

---
+++ src/main/java/co/th/best/springbootjenkins/SpringbootJenkinsApplication.java
@@ -0,0 +1,13 @@
+package co.th.best.springbootjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/th/best/springbootjenkins/SpringbootJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.th.best.springbootjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
